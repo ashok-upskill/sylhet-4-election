a8761c21a10a062ef864ced297f58f668f11bbc5
Ashok Bhattacharjee <ashokbhatt.designer@gmail.com>
2026-01-19 13:50:14 +0600

    🔧 Fix: Remove props from RecentProblems

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,18 @@ import { StatsSection } from '@/components/home/stats-section';
 import { UpazilaCards } from '@/components/home/upazila-cards';
 import { RecentProblems } from '@/components/home/recent-problems';
 import { CTASection } from '@/components/home/cta-section';
-import { getRecentProblems } from '@/lib/api'; // ডাটাবেস ফাংশন ইম্পোর্ট
 
-// পেজ কম্পোনেন্ট এখন async হবে কারণ আমরা ডাটা ফেচ করছি
-export default async function Home() {
-  // ডাটাবেস থেকে সাম্প্রতিক ৬টি সমস্যা নিয়ে আসা
-  const problems = await getRecentProblems(6);
-
+export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
       
-      {/* স্ট্যাটস সেকশন আপাতত স্ট্যাটিক থাকছে, পরে লাইভ করব */}
       <StatsSection />
       
       <UpazilaCards />
       
-      {/* রিয়েল ডাটা পাঠাচ্ছি */}
-      <RecentProblems problems={problems} />
+      {/* RecentProblems নিজেই data fetch করে */}
+      <RecentProblems />
       
       <CTASection />
     </main>
